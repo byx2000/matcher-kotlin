8d01aaee72e1bba9914ac2a3f33eeccffe3b6624
byx2000 <18023699313@163.com>
2024-03-21 20:39:01 +0800

    重构

--- src/main/kotlin/byx/matcher/Matcher.kt
+++ src/main/kotlin/byx/matcher/Matcher.kt
@@ -5,66 +5,106 @@ fun interface Matcher {
      * 从[index]位置开始解析字符串[s]，返回解析后的位置集合，用Sequence表示
      */
     fun parse(s: String, index: Int): Sequence<Int>
+}
 
-    /**
-     * 判断字符串[s]是否与当前模式匹配
-     */
-    fun match(s: String) = parse(s, 0).any { i -> i == s.length }
+/**
+ * 判断字符串[s]是否与当前模式匹配
+ */
+fun Matcher.match(s: String) = parse(s, 0).any { i -> i == s.length }
 
-    /**
-     * 连接两个Matcher
-     */
-    infix fun and(rhs: Matcher) = Matcher { s, index ->
-        parse(s, index).flatMap { i -> rhs.parse(s, i) }.distinct()
-    }
+/**
+ * 连接两个Matcher
+ */
+infix fun Matcher.and(rhs: Matcher) = Matcher { s, index ->
+    parse(s, index).flatMap { i -> rhs.parse(s, i) }.distinct()
+}
 
-    /**
-     * 将字符串[s]转换成Matcher，并与当前Matcher相连
-     */
-    infix fun and(s: String) = and(str(s))
+/**
+ * 将字符串[s]转换成Matcher，并与当前Matcher相连
+ */
+infix fun Matcher.and(s: String) = and(str(s))
 
-    /**
-     * 将字符[c]转换成Matcher，并与当前Matcher相连
-     */
-    infix fun and(c: Char) = and(ch(c))
+/**
+ * 将字符[c]转换成Matcher，并与当前Matcher相连
+ */
+infix fun Matcher.and(c: Char) = and(ch(c))
 
-    /**
-     * 使用or连接两个Matcher
-     */
-    infix fun or(rhs: Matcher) = Matcher { s, index ->
-        (parse(s, index) + rhs.parse(s, index)).distinct()
-    }
+/**
+ * 使用or连接两个Matcher
+ */
+infix fun Matcher.or(rhs: Matcher) = Matcher { s, index ->
+    (parse(s, index) + rhs.parse(s, index)).distinct()
+}
 
-    /**
-     * 将字符串[s]转换成Matcher，并使用or与当前Matcher相连
-     */
-    infix fun or(s: String) = or(str(s))
+/**
+ * 将字符串[s]转换成Matcher，并使用or与当前Matcher相连
+ */
+infix fun Matcher.or(s: String) = or(str(s))
 
-    /**
-     * 将字符[c]转换成Matcher，并使用or与当前Matcher相连
-     */
-    infix fun or(c: Char) = or(ch(c))
+/**
+ * 将字符[c]转换成Matcher，并使用or与当前Matcher相连
+ */
+infix fun Matcher.or(c: Char) = or(ch(c))
 
-    /**
-     * 将当前Matcher连续应用至少[minTimes]次
-     */
-    fun many(minTimes: Int = 0) = Matcher { s, index ->
-        sequence {
-            // 应用minTimes次
-            var seq = sequenceOf(index)
-            repeat(minTimes) {
-                seq = seq.flatMap { i -> parse(s, i) }.distinct()
+/**
+ * 将当前Matcher连续应用至少[minTimes]次
+ */
+fun Matcher.many(minTimes: Int = 0) = Matcher { s, index ->
+    sequence {
+        // 应用minTimes次
+        var seq = sequenceOf(index)
+        repeat(minTimes) {
+            seq = seq.flatMap { i -> parse(s, i) }.distinct()
+        }
+
+        val visited = HashSet<Int>()
+        seq.forEach { i ->
+            yield(i)
+            visited.add(i)
+        }
+
+        // 继续应用当前解析器，直到没有新的位置产生
+        val queue = ArrayDeque(visited)
+        while (!queue.isEmpty()) {
+            for (i in parse(s, queue.removeFirst())) {
+                if (!visited.contains(i)) {
+                    yield(i)
+                    visited.add(i)
+                    queue.addLast(i)
+                }
             }
+        }
+    }
+}
 
-            val visited = HashSet<Int>()
-            seq.forEach { i ->
-                yield(i)
-                visited.add(i)
-            }
+/**
+ * 将当前Matcher连续应用1次或多次
+ */
+fun Matcher.many1() = many(1)
 
-            // 继续应用当前解析器，直到没有新的位置产生
-            val queue = ArrayDeque(visited)
-            while (!queue.isEmpty()) {
+/**
+ * 将当前Matcher重复最少[minTimes]次，最多[maxTimes]次
+ */
+fun Matcher.repeat(minTimes: Int, maxTimes: Int) = Matcher { s, index ->
+    sequence {
+        // 应用minTimes次
+        var set = sequenceOf(index)
+        repeat(minTimes) {
+            set = set.flatMap { i -> parse(s, i) }.distinct()
+        }
+
+        val visited = HashSet<Int>()
+        set.forEach { i ->
+            yield(i)
+            visited.add(i)
+        }
+
+        // 继续应用直到maxTimes次
+        val queue = ArrayDeque(visited)
+        var times = minTimes
+        while (!queue.isEmpty() && times < maxTimes) {
+            val cnt = queue.size
+            repeat(cnt) {
                 for (i in parse(s, queue.removeFirst())) {
                     if (!visited.contains(i)) {
                         yield(i)
@@ -73,64 +113,24 @@ fun interface Matcher {
                     }
                 }
             }
+            times++
         }
     }
+}
 
-    /**
-     * 将当前Matcher连续应用1次或多次
-     */
-    fun many1() = many(1)
+/**
+ * 将当前解析器重复[times]次
+ */
+fun Matcher.repeat(times: Int) = repeat(times, times)
 
-    /**
-     * 将当前Matcher重复最少[minTimes]次，最多[maxTimes]次
-     */
-    fun repeat(minTimes: Int, maxTimes: Int) = Matcher { s, index ->
-        sequence {
-            // 应用minTimes次
-            var set = sequenceOf(index)
-            repeat(minTimes) {
-                set = set.flatMap { i -> parse(s, i) }.distinct()
-            }
-
-            val visited = HashSet<Int>()
-            set.forEach { i ->
-                yield(i)
-                visited.add(i)
-            }
-
-            // 继续应用直到maxTimes次
-            val queue = ArrayDeque(visited)
-            var times = minTimes
-            while (!queue.isEmpty() && times < maxTimes) {
-                val cnt = queue.size
-                repeat(cnt) {
-                    for (i in parse(s, queue.removeFirst())) {
-                        if (!visited.contains(i)) {
-                            yield(i)
-                            visited.add(i)
-                            queue.addLast(i)
-                        }
-                    }
-                }
-                times++
-            }
-        }
-    }
-
-    /**
-     * 将当前解析器重复[times]次
-     */
-    fun repeat(times: Int) = repeat(times, times)
-
-    /**
-     * 应用当前Matcher，并调用[mapper]生成下一个Matcher，继续应用下一个Mapper
-     */
-    fun flatMap(mapper: (String) -> Matcher) = Matcher { s, index ->
-        parse(s, index).flatMap { i ->
-            val matchStr = s.substring(index..<i)
-            mapper(matchStr).parse(s, i)
-        }.distinct()
-    }
+/**
+ * 应用当前Matcher，并调用[mapper]生成下一个Matcher，继续应用下一个Mapper
+ */
+fun Matcher.flatMap(mapper: (String) -> Matcher) = Matcher { s, index ->
+    parse(s, index).flatMap { i ->
+        val matchStr = s.substring(index..<i)
+        mapper(matchStr).parse(s, i)
+    }.distinct()
 }
 
 /**
